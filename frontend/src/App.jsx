6d9eae330b7d9c9de751bf2b27f1e78b685d4c51
Shrish <shrish.dollin1@gmail.com>
2024-08-24 15:27:17 +0530

    react components and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Header } from './pages/header/Header'
+import { Dashboard } from './pages/dashboard/Dashboard'
+import { Login } from './pages/auth/login/Login'
+import { SignUp } from './pages/auth/signup/SignUp'
 
 function App() {
   return (
-    <div>Hello</div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<SignUp/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </>
   )
 }
 
