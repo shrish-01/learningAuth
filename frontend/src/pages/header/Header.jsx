839507b5424d8f4731648661f0bb9934d51fb170
Shrish <shrish.dollin1@gmail.com>
2024-08-25 16:32:24 +0530

    creating inital navbar for not logged in users

--- frontend/src/pages/header/Header.jsx
+++ frontend/src/pages/header/Header.jsx
@@ -1,5 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import "./Header.css";
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand>Not-LoggedIn</Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/login" className='nav-link'>Login</Nav.Link>
+            <Nav.Link as={Link} to="/register" className='nav-link'>Sign Up</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
