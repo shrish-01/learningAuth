e963b419837d8cc2f2a592b5e3009287a361c654
Shrish <shrish.dollin1@gmail.com>
2024-09-12 01:17:02 +0530

    calling signup api from nodejs using react

--- frontend/src/pages/auth/signup/SignUp.jsx
+++ frontend/src/pages/auth/signup/SignUp.jsx
@@ -1,8 +1,11 @@
 import { Button, Form } from 'react-bootstrap';
 import "./SignUp.css";
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const SignUp = () => {
+
+  const navigate = useNavigate();
 
   const [ formData, setFormData ] = useState({
     email: '',
@@ -20,9 +23,31 @@ export const SignUp = () => {
 
   const handleSumbit = async(e) => {
     e.preventDefault();
-    console.log("Email: ", formData.email);
-    console.log("Name: ", formData.name);
-    console.log("Password: ", formData.password);
+    // console.log("Email: ", formData.email);
+    // console.log("Name: ", formData.name);
+    // console.log("Password: ", formData.password);
+
+    try {
+      const response = await fetch("http://localhost:3001/user/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await response.json();
+      console.log(result);
+      navigate("/login");
+    } catch (error) {
+      console.error(error.message);
+    } finally {
+      setFormData({
+        email: "",
+        name: "",
+        password: ""
+      })
+    }
   }
 
   return (
--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const signupRoute = require("./routes/signup");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/user", signupRoute);
 
