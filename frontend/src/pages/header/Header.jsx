18780c24550c02e9a063990014081abec318063e
Shrish Dollin <shrish.dollin1@gmail.com>
2024-09-29 12:58:01 +0000

    handle navbar according to user loggedin/not

--- frontend/src/pages/header/Header.jsx
+++ frontend/src/pages/header/Header.jsx
@@ -1,18 +1,36 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import "./Header.css";
 
 export const Header = () => {
+
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login") 
+  }
+
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
+      <Navbar bg={token ? "primary" : "dark"} variant='dark'>
         <Container>
-          <Navbar.Brand>Not-LoggedIn</Navbar.Brand>
+          <Navbar.Brand as={Link} to="">{token ? "LoggedIn" : "Not-LoggedIn"}</Navbar.Brand>
           <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/login" className='nav-link'>Login</Nav.Link>
-            <Nav.Link as={Link} to="/register" className='nav-link'>Sign Up</Nav.Link>
+            { token ? (
+              <>
+                <Nav.Link as={Link} to="/dashboard" className='nav-link'>Dashboard</Nav.Link>
+                <Nav.Link className='nav-link' onClick={handleLogout}>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login" className='nav-link'>Login</Nav.Link>
+                <Nav.Link as={Link} to="/register" className='nav-link'>Sign Up</Nav.Link>
+              </>
+            )}
           </Nav>
         </Container>
       </Navbar>
