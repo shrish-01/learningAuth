92c82073f7bec51579398ae6cb0138df34f2f467
Shrish Dollin <shrish.dollin1@gmail.com>
2024-10-02 16:16:06 +0000

    create jwt verify method

---
+++ src/utils/authMiddleware.js
@@ -0,0 +1,29 @@
+const { secretKey } = require("../config/jwt");
+const jwt = require("jsonwebtoken");
+
+export function authenticateToken(req, res, next) {
+    const authHeader = req.header("Authorization");
+    if(!authHeader) {
+        return res.status(401).json({
+            message: "Un-authorized, missing token!"
+        });
+    }
+
+    const [bearer, token] = authHeader.split(" ");
+    if(bearer !== "Bearer" || !token) {
+        return res.status(401).json({
+            message: "Un-authorized, invalid token format"
+        });
+    }
+
+    jwt.verify(token, secretKey, (err, user) => {
+        if(err) {
+            res.status(403).json({
+                message: "Forbidden: Invalid token"
+            });
+
+            req.user = user;
+            next();
+        }
+    });
+}
